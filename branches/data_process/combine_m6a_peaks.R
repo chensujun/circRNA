d5fada2ef332b8d74e9b5181608538ce636c9f88
sujunc <chensujunotl@gmail.com>
2018-12-19 13:18:34 -0500

    merge files and output results

--- branches/data_process/combine_m6a_peaks.R
+++ branches/data_process/combine_m6a_peaks.R
@@ -1,12 +1,9 @@
 ### combine_m6a_peaks.R ####################################################################
 # Description
 # Generates a matrix of combined m6a peaks 
-
-
 ### HISTORY #######################################################################################
 # Version       Date            Developer           Comments
 # 0.01          2018-12-15      Schen       
-
 ### NOTES #########################################################################################
 # named.bed
 # column 1 : chr
@@ -19,7 +16,6 @@
 # ...
 ### PREAMBLE ######################################################################################
 library(getopt);
-
 params <- matrix(
 	c(
 		'input_dir', 'i', '0', 'character',
@@ -48,3 +44,42 @@ if(is.null(opt$input_dir)) {
 flist <- list.files(path = opt$input_dir, pattern=opt$pattern, full.names = TRUE);
 fname <- gsub('.*/', '', flist);
 fname <- gsub(opt$pattern, '', fname);
+i <- 1;
+tmp <- read.table(flist[i], as.is = TRUE)[, 1:7];
+tmp <- unique(tmp[, c(1:4, 6)]);
+rownames(tmp) <- apply(tmp[, c(1:5)], 1, paste0, collapse = '_');
+mydata <- data.frame(matrix(nrow = nrow(tmp), ncol = length(fname)));
+rownames(mydata) <- rownames(tmp);
+colnames(mydata) <- fname;
+mydata[rownames(tmp), fname[i]] <- 1;
+for(i in seq(2, length(fname))){
+	tmp <- read.table(flist[i], as.is = TRUE)[, 1:7];
+	tmp <- unique(tmp[, c(1:4, 6)]);
+	rownames(tmp) <- apply(tmp[, c(1:5)], 1, paste0, collapse = '_');
+	mydata[rownames(tmp), fname[i]] <- 1;
+};
+mydata[is.na(mydata)] <- 0;
+save(mydata, file = paste0(Sys.Date(), '_peaks_uniq.rda'));
+#### create bed file, merge overlapping regions
+myinfo <- strsplit(rownames(mydata), '_');
+mybed <- data.frame(chr = gsub('_.*', '', rownames(mydata)), 
+	start = sapply(myinfo, function(x) x[2]),
+	end = sapply(myinfo, function(x) x[3]),
+	name = sapply(myinfo, function(x) x[4]),
+	score = 0,
+	strand = sapply(myinfo, function(x) x[5])
+	);
+mybed$start <- as.numeric(as.vector(mybed$start));
+mybed <- mybed[order(mybed$chr, mybed$start), ];
+mybed$start <- as.vector(mybed$start);
+mybed$end <- as.vector(as.numeric(as.vector(mybed$end)));
+mybed <- cbind(mybed, mydata);
+write.table(mybed, paste0(Sys.Date(), '_peaks_uniq.bed'), quote = FALSE, row.names = FALSE, col.names = FALSE, sep = '\t');
+cmd <- paste0('/mnt/work1/software/bedtools/2.27.1/bin/bedtools merge -s -c 4,5,6,', paste0(seq(7, 69), collapse=','), ' -o distinct,distinct,distinct,', paste0(rep('max', 63), collapse = ','), ' -i ', paste0(Sys.Date(), '_peaks_uniq.bed'), '> ', paste0(Sys.Date(), '_peaks_merge.bed'));
+system2('/bin/bash', args = c('-c', shQuote(cmd)));
+cmd1 <- paste0('cut -f 1,2,3,4,5,6 ', paste0(Sys.Date(), '_peaks_merge.bed'), '> ', paste0(Sys.Date(), '_peaks_merge_anno.bed'))
+system2('/bin/bash', args = c('-c', shQuote(cmd1)));
+#### read in the new file
+mdat <- read.table(paste0(Sys.Date(), '_peaks_merge.bed'), as.is = TRUE);
+colnames(mdat) <- fname;
+save(mdat, file = paste0(Sys.Date(), '_peaks_merge.rda'));
